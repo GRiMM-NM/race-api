342d0b3d18dc709386bf473fb54b1a317dfb2a92
m.perrier <maxence.perrier@epfedu.fr>
2026-03-11 16:17:59 +0100

    DEBUT PROJET API

--- src/main/java/com/takima/race/runner/controllers/RunnerController.java
+++ src/main/java/com/takima/race/runner/controllers/RunnerController.java
@@ -2,12 +2,16 @@ package com.takima.race.runner.controllers;
 
 import com.takima.race.runner.entities.Runner;
 import com.takima.race.runner.services.RunnerService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/runners")
@@ -26,5 +30,10 @@ public class RunnerController {
     @GetMapping("/{id}")
     public Runner getById(@PathVariable Long id) {
         return runnerService.getById(id);
+    }
+
+    @PostMapping
+    public Runner create(@RequestBody Runner runner) {
+        return runnerService.create(runner);
     }
 }
